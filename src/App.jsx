2e44b04bfbfd686926c37a88148711ed949184fb
Joelnicolass <joelnicolass@gmail.com>
2023-08-31 20:18:34 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import Tarea1 from "./tarea/Tarea1";
 import Estados from "./temario/Estados";
 import Contador from "./tarea/Contador";
-import CardPresentacion from "./ejemplos/CardPresentacion";
 import { grupo } from "./helpers/grupos";
 import Efectos from "./temario/Efectos";
 
 const App = () => {
   return (
     <>
-      <CardPresentacion grupo={grupo} />
       {/* <Contador /> */}
-      {/* <Efectos /> */}
+      <Efectos />
     </>
   );
 };
